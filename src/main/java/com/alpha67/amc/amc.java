2dac67d4b0522e39f68cf76db179583def586fe8
vultorio67 <68811816+vultorio67@users.noreply.github.com>
2021-09-30 09:45:39 +0200

    update

--- src/main/java/com/alpha67/amc/amc.java
+++ src/main/java/com/alpha67/amc/amc.java
@@ -15,6 +15,7 @@ public class amc {
     {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+	int a = "salut";
     }
 
     public void setup(FMLCommonSetupEvent e)
