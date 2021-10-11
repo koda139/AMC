3aa5756bd2aa36cdab34af605ed51d4a93038e7a
vultorio67 <68811816+vultorio67@users.noreply.github.com>
2021-10-11 19:03:58 +0200

    add title window

--- src/main/java/com/alpha67/amc/amc.java
+++ src/main/java/com/alpha67/amc/amc.java
@@ -1,6 +1,9 @@
 package com.alpha67.amc;
 
 
+import net.minecraft.client.MainWindow;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -15,6 +18,7 @@ public class amc {
     {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
     }
 
     public void setup(FMLCommonSetupEvent e)
@@ -22,8 +26,26 @@ public class amc {
 
     }
 
-    public void clientSetup(FMLCommonSetupEvent e)
+    @SubscribeEvent
+    public void clientSetup(FMLClientSetupEvent e)
     {
 
     }
+
+    @SubscribeEvent
+    public void onClientSetup(final FMLClientSetupEvent e){
+        e.getMinecraftSupplier().get().execute(this::updateTitle);
+    }
+
+    private void updateTitle(){
+        String Name = "Alpha67";
+        final MainWindow window = Minecraft.getInstance().getWindow();
+        window.setTitle(Name);
+
+
+    }
+
+
+
 }
+
