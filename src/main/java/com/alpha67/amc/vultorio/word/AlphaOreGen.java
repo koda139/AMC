1568a1c2b67c274b2feb78144fe684f741039f88
vultorio67 <68811816+vultorio67@users.noreply.github.com>
2021-12-07 08:56:03 +0100

    Update AlphaOreGen.java

--- src/main/java/com/alpha67/amc/vultorio/word/AlphaOreGen.java
+++ src/main/java/com/alpha67/amc/vultorio/word/AlphaOreGen.java
@@ -2,6 +2,9 @@ package com.alpha67.amc.vultorio.word;
 
 import com.alpha67.amc.vultorio.init.BlockInitVultorio;
 import com.alpha67.amc.vultorio.init.ItemInitVultorio;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.StateContainer;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.GenerationStage;
@@ -10,6 +13,7 @@ import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.template.RuleTest;
 import net.minecraft.world.gen.placement.ConfiguredPlacement;
 import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.TopSolidRangeConfig;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -18,15 +22,13 @@ public class AlphaOreGen {
     public static void generateOres(final BiomeLoadingEvent event) {
         if (!(event.getCategory().equals(Biome.Category.NETHER) || event.getCategory().equals(Biome.Category.THEEND))) {
             generateOre(event.getGeneration(), OreFeatureConfig.FillerBlockType.NATURAL_STONE,
-                    BlockInit.EXAMPLE_ORE.get().getDefaultState(), 5, 15, 30, 10);
+                    BlockInitVultorio.alphariteore.get().getStateDefinition(), 5, 15, 30, 10);
         }
     }
 
-    private static void generateOre(BiomeGenerationSettingsBuilder settings, RuleTest fillerType, BlockState state,
+    private static void generateOre(BiomeGenerationSettingsBuilder settings, RuleTest fillerType, StateContainer<Block, BlockState> state,
                                     int veinSize, int minHeight, int maxHeight, int amount) {
-        settings.withFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
-                Feature.ORE.withConfiguration(new OreFeatureConfig(fillerType, state, veinSize))
-                        .withPlacement(Placement.RANGE.configure(new TopSolidRangeConfig(minHeight, 0, maxHeight)))
-                        .square().func_242731_b(amount));
+        settings.getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.configured(new OreFeatureConfig(fillerType, state, veinSize))
+                .place(Placement.RANGE.configured(new TopSolidRangeConfig(minHeight, 0, maxHeight))));
     }
 }
